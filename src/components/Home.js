e306c7019db894eba93b22a3229d5b83a22392ec
Anselm Papendorf <anselm.papendorf@gmail.com>
2022-09-02 09:01:53 +0200

    pokelist now with pictures

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Router, NavLink, Routes, Route } from "react-router-dom";
 import axios from "axios";
+import { Button } from "@mui/material";
+import { DotLoader } from "react-spinners";
 import { useEffect, useState } from "react";
 
 const Home = () => {
@@ -9,29 +11,13 @@ const Home = () => {
 
   const fetchMyAPI = async () => {
     let response = await axios
-      .get("https://pokefight-backend.onrender.com/pokemon/")
+      .get("http://localhost:3000/pokemon/")
       .then((r) => {
         setIsLoading(false);
         setPokeList(r);
         console.log("all pokemons as returned from render.com: ", r);
       });
-    // let resp = response.json();
   };
-  /*
-  const fetchPokeSprites = () => {
-    for (let i = 0; i < 809; i++) {
-      //      let p = pokeList[i];
-      //    let n = p.name;
-      let response = axios
-        .get(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-        .then((r) => {
-          console.log("pokemons ", " looks like ", r.sprites.front_default);
-        });
-      // let resp = response.json();
-    }
-    
-  };
-*/
   useEffect(() => {
     setIsLoading(true);
     fetchMyAPI();
@@ -39,19 +25,27 @@ const Home = () => {
   }, []);
 
   if (isLoading) {
-    return <h1> Gotta Load 'em All.........</h1>;
+    return (
+      <>
+        <h1> Gotta Load 'em All.........</h1>
+        <DotLoader />
+      </>
+    );
   } else {
     return (
       <div>
+        <Button>Heres a button</Button>
         <h1>Here's our Pokemon list</h1>
+        <Button>And Heres another</Button>
         {pokeList?.data?.map((e, i) => (
-          <span key={i}>
-            {" "}
-            {e.name.english}
-            <img
-              src={`https://pokeapi.co/api/v2/pokemon/${e.name.english.toLowerCase}`}
-            />
-          </span>
+          <NavLink to={`/pokemon/${e.id}`} key={i}>
+            <Button variant="outlined" key={i}>
+              <span key={i}>
+                {e.id} {e.name.english}
+                <img alt="" src={e.pictureFront} />
+              </span>
+            </Button>
+          </NavLink>
         ))}
       </div>
     );
---
+++ src/components/pokemon.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Router, NavLink, Routes, Route, useParams } from "react-router-dom";
+import axios from "axios";
+import { Button } from "@mui/material";
+import { DotLoader } from "react-spinners";
+import { useEffect, useState } from "react";
+
+const Pokemon = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pokemon, setPokemon] = useState();
+  const params = useParams();
+
+  console.log("Displaying pokeomn ", params);
+  const fetchMyAPI = async () => {
+    let response = await axios
+      .get(`http://localhost:3000/pokemon/${params.id}`)
+      .then((r) => {
+        setIsLoading(false);
+        console.log("single pokemons as returned from backend ", r.data);
+        setPokemon(r.data);
+      });
+    // let resp = response.json();
+  };
+  useEffect(() => {
+    setIsLoading(true);
+    fetchMyAPI();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <h1> Gotta Load a single one.........</h1>
+        <DotLoader />
+      </>
+    );
+  } else {
+    return (
+      <div>
+        <h2>
+          Here's Pokemon {params.id}, {pokemon?.name?.english}{" "}
+        </h2>
+        <img src={pokemon?.pictureBack} alt="" />
+        <img src={pokemon?.pictureFront} alt="" />
+        &nbsp;{pokemon.name.french}
+        &nbsp;{pokemon.name.japanese}
+        &nbsp;Type: {pokemon.type[0]} &nbsp; HP: {pokemon.base.HP}
+        &nbsp; Attach: {pokemon.base.Attack}
+        &nbsp;Defense: {pokemon.base.Defense}
+        <Button variant="outlined"> I choose you....</Button>
+      </div>
+    );
+  }
+};
+
+export default Pokemon;
